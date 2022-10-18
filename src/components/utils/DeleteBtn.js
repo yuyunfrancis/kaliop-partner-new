118bdc093aa3f17cfadfddfe1b21c860d1af67f6
yuyunfrancis <francisberi@yahoo.com>
2022-10-18 15:48:20 +0100

    home-screen-done

--- src/components/utils/DeleteBtn.js
+++ src/components/utils/DeleteBtn.js
@@ -1,19 +1,18 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React from 'react';
 
-import { COLORS } from '../../contants'
-
-const DeleteBtn = ({ text, onPress, btnStyle }) => {
+import {COLORS} from '../../constants';
+const DeleteBtn = ({text, onPress, btnStyle}) => {
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={[styles.deleteBtn, btnStyle]}>
         <Text style={styles.deleteBtnText}>Delete</Text>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default DeleteBtn
+export default DeleteBtn;
 
 const styles = StyleSheet.create({
   deleteBtn: {
@@ -25,12 +24,12 @@ const styles = StyleSheet.create({
     paddingBottom: 8,
     borderRadius: 12,
     borderWidth: 2,
-    borderColor: COLORS.red
+    borderColor: COLORS.red,
 
     // marginRight: 20,
   },
   deleteBtnText: {
     fontSize: 15,
-    color: COLORS.red
-  }
-})
+    color: COLORS.red,
+  },
+});
